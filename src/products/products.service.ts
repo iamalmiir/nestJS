0657f4526a96e8f4acf95c7dd09c4a5040f754e5
iamalmiir <iamalmiir@gmail.com>
2021-10-29 19:55:29 -0400

    improvements

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,14 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './product.model';
 
 @Injectable()
 export class ProductsService {
-  products: Product[] = [];
+  private products: Product[] = [];
 
   insertProduct(title: string, desc: string, price: number) {
     const prodId = Math.random().toString();
     const newProduct = new Product(prodId, title, desc, price);
     this.products.push(newProduct);
     return prodId;
+  }
+
+  getProducts() {
+    return [...this.products];
+  }
+
+  getProduct(pId: string) {
+    const product = this.products.find((prod) => prod.id === pId);
+    if (!product) {
+      return new NotFoundException('No product found');
+    }
+    return { ...product };
   }
 }
